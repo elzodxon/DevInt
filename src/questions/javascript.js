5a3a8dba305664f8356ebb26398fc21165a82d85
Elzodxon Sharofaddinov <elzodxon@gmail.com>
2023-11-12 21:50:54 +0500

    feat: javascript number

--- src/questions/javascript.js
+++ src/questions/javascript.js
@@ -3,14 +3,14 @@ const questions = [
         id: 1000,
         question: 'What is JavaScript?',
         answer: 'JavaScript is a scripting language that enables you to create dynamically updating content, control multimedia, animate images, and pretty much everything else.',
-        position: 0,
+        position: 1,
         tags: ["javascript"]
     },
     {
         id: 1001,
         question: 'What is the difference between "resetting" and "normalizing" CSS? Which would you choose, and why?',
         answer: 'Resetting removes all native browser styling. Normalizing makes the default styling consistent across browsers. I would choose normalizing because it is more consistent and easier to work with.',
-        position: 0,
+        position: 2,
         tags: ["css"]
     },
 
